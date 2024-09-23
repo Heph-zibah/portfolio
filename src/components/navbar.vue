ac1230e8802a1433dff764eef2f873c4017601aa
oaDaramola <oluwatosinhephzibah@gmail.com>
2024-09-23 13:32:23 +0100

    woorked on the navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -24,43 +24,52 @@ onMounted(() => {
 </script>
 
 <template>
-    <header class="  px-5 py-6 border-b border-blue-200 absolute w-full" >
+    <header class=" px-5 md:px-14 py-10   w-full" >
         <nav class="flex items-center justify-between">
-            <h1 class="text-3xl font-logo dark:text-white">oadaramola
-            </h1>
+            <img src="/src/assets/cropped-logo.png" alt="" class="w-28" v-if="!isDark">
+            <img src="/src/assets/black-logo.png" alt="" class="w-28" v-if="isDark">
+            <ul class="md:flex  hidden gap-12 items-center border border-gray-400 rounded-full px-4 py-3 dark:text-[#868C98] dark:bg-white dark:bg-opacity-10">
+                <li>Home</li>
+                <li>About</li>
+                <li>Resume</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+                <li>Blog</li>     
+            </ul>
             <div class="flex items-center gap-5">
                 <button @click="isToggle()"> <i :class="isDark ? 'fa-regular fa-lightbulb text-2xl p-2 dark:text-white' : 'fa-solid fa-lightbulb text-2xl p-2 dark:text-white'"></i></button>
-                <i :class="isNavbar ? 'fa-solid fa-xmark text-2xl p-2 dark:text-white navbar' : 'fa-solid fa-bars text-2xl p-2 dark:text-white'" @click="isToggleNavbar"></i>
-                
+                <i class="md:hidden" :class="isNavbar ? 'fa-solid fa-xmark text-2xl p-2 dark:text-white navbar' : 'fa-solid fa-bars text-2xl p-2 dark:text-white'" @click="isToggleNavbar"></i>
+                <div class="rounded-full bg-white py-3.5 px-10 hidden md:block font-semibold" :class="isDark? 'border-0' : 'border border-black'">
+                    <button>Get in touch</button>
+                </div>
             </div>
         </nav>
-        <div class=" w-[60%] h-full absolute left-0 top-0  " v-if="isNavbar">
-            <div class="bg-black dark:bg-white dark:bg-opacity-50 bg-opacity-50 h-screen">
-                    <div>
-                        <img src="../assets/avatar-cropped.jpg" alt="" class="h-full w--full">
-                    </div>
+        <div class="w-full h-full fixed left-0 top-0  max-h-full bg-black dark:bg-white bg-opacity-40 dark:bg-opacity-70" v-if="isNavbar">
+            <div class="w-[50%]">
+                <div class="bg-black dark:bg-white h-screen dark:text-black text-xs pt-14 z-[1000]">
+                    
                     <div class=" py-6 text-white space-y-3">
-                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer">
+                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer dark:text-black">
                             <i class="fa-solid fa-house-signal"></i>
                             <span>HOME</span>
                         </div>
-                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer">
+                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer dark:text-black">
                             <i class="fa-regular fa-user"></i>
                             <span>ABOUT</span>
                         </div>
-                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer">
+                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer dark:text-black">
                             <i class="fa-regular fa-id-card"></i>
                             <span>RESUME</span>
                         </div>
-                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer">
+                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer dark:text-black">
                             <i class="fa-solid fa-business-time"></i>
                             <span>PORTFOLIO</span>
                         </div>
-                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer">
+                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer dark:text-black">
                             <i class="fa-regular fa-envelope"></i>
                             <span>CONTACT</span>
                         </div>
-                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer">
+                        <div class="flex items-center gap-5 px-5 py-2 hover:bg-gray-900 cursor-pointer dark:text-black">
                             <i class="fa-solid fa-file-signature"></i>
                             <span>BLOG</span>
                         </div>
@@ -69,23 +78,24 @@ onMounted(() => {
                     <div class="px-5 space-y-3">
                         <div class="flex items-center justify-between">
                             <a class="p-1 hover:scale-125 transition-all duration-500" href="https://github.com/Heph-zibah">
-                                <i class="fa-brands text-white fa-github"></i>
+                                <i class="fa-brands text-white dark:text-black fa-github"></i>
                             </a>
                             <a class="p-1 hover:scale-125 transition-all duration-500" href="">
-                                <i class="fa-brands text-white fa-medium"></i>
+                                <i class="fa-brands text-white dark:text-black fa-medium"></i>
                             </a>
                             <a class="p-1 hover:scale-125 transition-all duration-500" href="">
-                                <i class="fa-brands text-white fa-x-twitter"></i>
+                                <i class="fa-brands text-white dark:text-black fa-x-twitter"></i>
                             </a>
                             <a class="p-1 hover:scale-125 transition-all duration-500" href="">
-                                <i class="fa-brands text-white fa-linkedin-in"></i>
+                                <i class="fa-brands text-white dark:text-black fa-linkedin-in"></i>
                             </a>
                             <a class="p-1 hover:scale-125 transition-all duration-500" href="">
-                                <i class="fa-brands text-white fa-facebook-f"></i>
+                                <i class="fa-brands text-white dark:text-black fa-facebook-f"></i>
                             </a>
                         </div>
-                        <p class="text-xs text-white">2024 © oadaramola.All Right Reserved.</p>
+                        <p class="text-xs text-white dark:text-black">2024 © oadaramola.All Right Reserved.</p>
                     </div>
+            </div>
             </div>
         </div>
     </header>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,26 @@ import VueTypewriterEffect from "vue-typewriter-effect";
 
 <template>
     <Navbar/>
-    <main class="flex flex-col gap-5 items-center justify-center   h-screen">
-        <h1 class="font-bold text-7xl  text-center mx-auto leading-[80px]">OLUWATOSIN DARAMOLA</h1>
-        <p class="text-2xl">I am a <vue-typewriter-effect :strings="['Frontend Developer']" cursorClassName="text-white" /></p>
-        
+    <main class=" h-screen dark:text-white my-5">
+       <section class="flex flex-col-reverse md:flex-row items-center ">
+
+         <div class=" px-5 md:px-0 md:ml-14 max-w-[635px] my-5 md:my-0">
+            <span class="text-sm md:text-base">Hey there, my name is</span>
+            <h1 class="font-bold text-2xl md:text-5xl leading-[30px] md:leading-[60px]">OLUWATOSIN DARAMOLA</h1>
+            <p class=" text-xl md:text-4xl">I am a <vue-typewriter-effect :strings="['Frontend Developer']" cursorClassName="text-white" /></p>
+            <p class="text-sm md:text-lg">I'm a passionate frontend developer with experience in creating beautiful and functional web applications. I specialize in building responsive, accessible, and user-friendly interfaces using modern technologies like Vue.js, 
+    Tailwind CSS, and JavaScript. My goal is to turn ideas into digital experiences that make a positive impact.</p>
+         </div>
+         <div>
+            <img src="/src/assets/avatar.png" alt="" class="w10">
+         </div>
+       </section>
+        <section class="flex items-center justify-between px-5 md:px-14 py-5 bg-black bg-opacity-10 dark:bg-white dark:bg-opacity-10">
+            <img src="../assets/HTML.png" alt="">
+            <img src="../assets/CSS.png" alt="">
+            <img src="../assets/Javascript.png" alt="">
+            <img src="../assets/Tailwind.png" alt="" class="w-10">
+            <img src="../assets/Vue.png" alt="">
+        </section>
     </main>
 </template>
